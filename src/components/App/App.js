af39e2368eb3c80ac99a7cdbbdd23e475bf8bf3d
teesloane <tylersloane@gmail.com>
2016-12-14 21:54:20 -0500

    Midi output works!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
 import OmeBtn from '../OmeBtn/OmeBtn';
+import './App.css';
 
 class App extends Component {
+
+  state = {
+    midi: undefined,
+    midiInputs: undefined,
+    midiOutputs: undefined, 
+    selectedPort: undefined,
+  }
 
   componentDidMount() {
     this.getMidiAccess()
@@ -10,32 +17,45 @@ class App extends Component {
 
   /**
    * getMidiAccess checks if the browser has midi --> yes? run midiSuccess.
-   * @memberOf App
    */
   getMidiAccess() {
-    // request MIDI access
     if (navigator.requestMIDIAccess) {
       navigator.requestMIDIAccess({
-        sysex: false // this defaults to 'false' and we won't be covering sysex in this article. 
+        sysex: false, // this defaults to 'false' and we won't be covering sysex in this article. 
       }).then( this.midiSuccess, () => {console.log('midi failed')});
     } else {
       alert("No MIDI support in your browser.");
     }
   }
 
-  
   /**
    * Runs when getMidiAccess succeeds (browser has midi)
-   * @memberOf App
    */
-  midiSuccess(midiAccess) {
-    console.log('midi is up: ', midiAccess)
+  midiSuccess = (midiAccess) => {
+    // midiAccess.outputs is a MAP --> so you grab the [1] --> the actual value, since [0] is just the id.
+    let inputs = [];
+    let outputs = [];
+    for (let midiOutput of midiAccess.outputs) { outputs.push(midiOutput[1]) }
+    for (let midiInput of midiAccess.inputs) { inputs.push(midiInput[1]) }
+
+    this.setState({
+      midi: midiAccess,
+      midiInputs: inputs,
+      midiOutputs: outputs,
+      selectedOutput: outputs[0] // make this set-able.
+    })
+  }
+
+  playNote = () => {
+    var noteOnMessage = [0x90, 70, 0x7f];    // note on, middle C, full velocity
+    this.state.selectedOutput.send( noteOnMessage );  //omitting the timestamp means send immediately.
+    this.state.selectedOutput.send( [0x80, 66, 0x40], window.performance.now() + 1000.0 ); // Inlined array creation- note off, middle C,  
   }
 
   render() {
     return (
       <div className="App">
-        <OmeBtn />
+        <OmeBtn playNote={this.playNote} />
       </div>
     );
   }
--- src/components/OmeBtn/OmeBtn.js
+++ src/components/OmeBtn/OmeBtn.js
@@ -4,64 +4,13 @@
  * possibly: turn "on" the other notes around it...
  */
 
-import React, { Component, PropTypes } from 'react';
+
+import React from 'react';
 import './OmeBtn.css';
 
-class OmeBtn extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      messageData: new Uint8Array(3)
-    }
-  }
-
-  sendNoteOn(note, velocity, output)  {
-    console.log('sendNoteOn invoked: ', note, velocity, output);
-    this.sendMessage(0x90, note, velocity, output);
-  }
-
-  sendNoteOff(note, velocity, output) {
-    this.sendMessage(0x80, note, velocity, output); 
-  } 
-
-  sendMessage(type, data1, data2, output) {
-    console.log(type, data1, data2, output);
-    this.setState.messageData[0] = type;
-    this.setState.messageData[1] = data1;
-    this.setState.messageData[2] = data2;
-
-    // Send the MIDI message immediately
-    output.send(this.state.messageData);
-  }
-
-  render() {
-    // conditonal styles
-    const OmeBtnStyle = {
-      backgroundColor: this.props.color && this.props.color
-    }
-
-    return (
-      <main 
-        style={OmeBtnStyle}
-        className={`OmeBtn`}
-        onClick={() => this.sendNoteOn(142, 64, 1) } 
-      />
-    );
-  }
-}
-
-OmeBtn.propTypes = {
-  color: PropTypes.string,
-  onClick: PropTypes.func,
-  note: PropTypes.string,
-  gate: PropTypes.number,
-  velocity: PropTypes.number
+const OmeBtn = props => {
+  const OmeBtnStyle = { backgroundColor: props.color && props.color }
+  return  <main style={OmeBtnStyle} className={`OmeBtn`} onClick={props.playNote } /> 
 };
-
-OmeBtn.defaultProps = {
-  note: "C",
-  gate: 64,
-}
 
 export default OmeBtn;
