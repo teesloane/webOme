a76785bf08f644b56fedd96ae417432fe6b9c72b
teesloane <tylersloane@gmail.com>
2017-01-07 12:11:41 -0500

    update midi image modal

--- src/components/Modals/NoMidi/NoMidi.js
+++ src/components/Modals/NoMidi/NoMidi.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './NoMidi.css';
+import React from 'react'
+import synth3 from './assets/synth3_noshadow.png'
+import './NoMidi.css'
 
 const NoMidi = props => {
   return (
     <div className="NoMidi">
       <div className="NoMidi-text">You don't have any midi devices! Sadly <em>WebOme</em> cannot function without some cool doo-dad midi devices along-side it.</div> 
-      <div className="NoMidi-image-carousel" />
+      <img className="NoMidi-image-synth" src={synth3} />
     </div>
   );
 };
